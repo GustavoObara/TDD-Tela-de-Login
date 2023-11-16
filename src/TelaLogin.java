ead3673c46d7885c9111015b6f81a1330d5fbfe7
GustavoObara <luizobara@gmail.com>
2023-11-16 20:10:20 -0300

    Adicionando a documentação

--- src/TelaLogin.java
+++ src/TelaLogin.java
@@ -1,7 +1,25 @@
+/**
+ * Representa a tela de login da aplicação.
+ */
 public class TelaLogin {
+
+    /**
+     * Nome de usuário permitido para login.
+     */
     private String login = "LuizObara";
+
+    /**
+     * Senha permitida para login.
+     */
     private String senha = "Abcd1234!";
 
+    /**
+     * Método para autenticar um usuário com base no nome de usuário e senha fornecidos.
+     *
+     * @param usuarioDigitado Nome de usuário digitado pelo usuário.
+     * @param senhaDigitada   Senha digitada pelo usuário.
+     * @return true se o nome de usuário e a senha correspondem aos permitidos, false caso contrário.
+     */
     public boolean login(String usuarioDigitado, String senhaDigitada) {
         return login.equals(usuarioDigitado) && senha.equals(senhaDigitada);
     }
--- src/TelaLoginTeste.java
+++ src/TelaLoginTeste.java
@@ -1,10 +1,25 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
+/**
+ * Classe de teste para a funcionalidade de login da TelaLogin.
+ */
 public class TelaLoginTeste {
+
+    /**
+     * Testa o método de login da TelaLogin com um usuário e senha válidos.
+     */
     @Test
     public void loginTeste() {
+        // Criar uma instância da classe TelaLogin
         TelaLogin telaLogin = new TelaLogin();
-        Assertions.assertTrue(telaLogin.login("LuizObara","Abcd1234!"));
+
+        // Testar o método login com um usuário e senha válidos
+        boolean resultado = telaLogin.login("LuizObara", "Abcd1234!");
+
+        // Verificar se o login foi bem-sucedido
+        Assertions.assertTrue(resultado, "O login deve ser bem-sucedido com usuário e senha válidos.");
+
+        // Você também pode adicionar mais testes aqui para diferentes cenários
     }
 }
